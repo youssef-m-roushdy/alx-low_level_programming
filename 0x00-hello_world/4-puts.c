9d1533af8b3b38b7f2a1cbb69f469f07a99cebb1
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-10 09:45:50 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - where program run
  *
- * Return: Always 0 (success)
- */
+ * Return - End of program
+ * */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
