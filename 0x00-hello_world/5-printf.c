bdb7772c2ab2cccaf6ef6e0237ac16e87ccc18cb
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-10 10:48:59 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- * main - this is the main function
+
+/**
+ * main - Entry point of the program
  *
- * Return: 0 when successfull
+ * Description: This function prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
