c17a89f85ead216520c3ee325af61283fc621ad2
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-16 13:49:57 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,32 +10,35 @@ while (counter1 <= 9)
 while (counter2 <= 9)
 {
 num = counter1 * counter2;
+if (num == 0)
+{
+_putchar(num + '0');
+counter2++;
+continue;
+}
+else if (num <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+}
 if (num > 9)
 {
 _putchar((num / 10) + '0');
 _putchar((num % 10) + '0');
-if (counter2 == 9)
-_putchar('\n');
-else
-{
-_putchar(',');
-_putchar(' ');
-}
 }
 else
 {
 _putchar(num + '0');
-if (counter2 == 9)
-_putchar('\n');
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
 }
 counter2++;
 }
+_putchar('\n');
 counter2 = 0;
 counter1++;
 }
