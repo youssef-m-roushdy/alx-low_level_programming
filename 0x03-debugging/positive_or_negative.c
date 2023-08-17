91b78aa036784847abcbb7c79e3c0280b4252dc0
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-17 12:04:45 -0700

    Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
 #include "main.h"
-
+/**
+ * positive_or_negative - Determines the sign of an integer.
+ * @i: The integer to be evaluated.
+ *
+ * This function prints whether @i is positive, negative, or zero.
+ */
 void positive_or_negative(int i)
 {
 if (i > 0)
