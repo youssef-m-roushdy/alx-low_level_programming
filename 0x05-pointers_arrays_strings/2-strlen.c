292af18fa82bfa180d7155ef0ded4138aa292cc6
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-22 10:28:14 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the input string.
+ *
+ * This function takes a pointer to
+ * a null-terminated string @s and calculates
+ * its length using the standard library function `strlen`.
+ * It returns the length
+ * of the string.
+ *
+ * Return: Length of the string.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
