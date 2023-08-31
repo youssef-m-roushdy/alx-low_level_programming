a9eea7fc9786f95bb07d85886f066a25a475ada5
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-08-30 18:24:36 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a newline using recursion
+ * @s: The string to be printed
+ */
 void _puts_recursion(char *s)
 {
 _putchar(*s);
