2150f5a7745fbc25535860ff3d2c6b361463cc93
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-01 20:13:07 -0700

    It ain't what they call you, it's what you answer to

--- 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ *
+ * Description:
+ * This program checks if any additional
+ * command-line arguments (excluding the
+ * program name itself) are provided.
+ * If not, it prints the name of the program.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
+int main(int argc, char *argv[])
 {
 	if (argc == 1)
-	printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
