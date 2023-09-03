feda61b138955d2d1e5fde9535d54b1ec86909d8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-02 19:09:11 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,14 @@
 #include "main.h"
-
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This is the main function of the program.
+ * It prints a quote by Kevin Mitnick
+ * using the _puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
