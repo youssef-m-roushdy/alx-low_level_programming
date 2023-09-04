9b569c3fda9d84048c4af323444864096eefdd66
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-04 05:31:15 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,25 +11,27 @@
  */
 int **alloc_grid(int width, int height)
 {
+int i, j;
+int **arr;
 if (width <= 0 || height <= 0)
-return (NULL);
-int **arr = (int **)malloc(height * sizeof(int *));
+return (NULL); 
+arr = (int **)malloc(height * sizeof(int *));
 if (arr == NULL)
 return (NULL);
-for (int i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
 arr[i] = (int *)malloc(width * sizeof(int));
 if (arr[i] == NULL)
 {
-for (int j = 0; j < i; j++)
+for (j = 0; j < i; j++)
 free(arr[j]);
 free(arr);
 return (NULL);
 }
 }
-for (int i = 0; i < height; i++)
+for (i = 0; i < height; i++)
 {
-for (int j = 0; j < width; j++)
+for (j = 0; j < width; j++)
 {
 arr[i][j] = 0;
 }
