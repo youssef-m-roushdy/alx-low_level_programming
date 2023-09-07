cbbb2d50f98b753916f98f172f44c9f8d422423a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-07 06:14:53 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		nconc[i] = s1[i];
 	if (len2 > n)
 		for (j = 0; j < n; j++)
