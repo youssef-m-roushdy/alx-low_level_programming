4c3d243acf240c764a1494e74c24091f1af35b55
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-07 06:21:17 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,46 @@
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * string_nconcat - Concatenates the first n characters of two
- * strings into a new string.
- * @s1: The first string to concatenate.
- * @s2: The second string to concatenate.
- * @n: The number of characters from s2 to concatenate.
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: A pointer to the newly created concatenated string,
- * or NULL if memory allocation fails
- *         or if s1 or s2 is NULL.
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1, len2;
-	char *nconc;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (len2 > n)
-		nconc = malloc((len1 + n + 1) * sizeof(char));
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		nconc = malloc((len1 + len2 + 1) * sizeof(char));
-	if (!nconc)
-	{
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
 	}
-	for (i = 0; i < len1; i++)
-		nconc[i] = s1[i];
-	if (len2 > n)
-		for (j = 0; j < n; j++)
-			nconc[i++] = s2[j];
-	else
-		for (j = 0; j < len2; j++)
-			nconc[i++] = s2[j];
-	nconc[i] = '\0';
-	return (nconc);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
