83d79920bf256f61cf9ab2ceacb98992f9f7c9d8
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-11 06:35:10 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Defines a structure to represent information
+ * about a dog.
+ * @name: Pointer to a character string for the dog's name.
+ * @age: Age of the dog as a floating-point number.
+ * @owner: Pointer to a character string for the dog's owner.
+ *
+ * Description: This structure contains information about
+ * a dog, including its
+ * name, age, and the name of its owner.
+ */
 struct dog{char *name; float age; char *owner;};
 #endif
