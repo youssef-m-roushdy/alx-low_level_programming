c1e8663e98a3ea18350f5993ee4d6fb5149a6095
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-25 11:05:00 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - Free memory allocated for a linked list of integers.
+ * @head: A pointer to the head of the linked list.
+ */
+void free_listint(listint_t *head)
+{
+	while (head)
+	{
+		free(head);
+		head = head->next;
+	}
+}
