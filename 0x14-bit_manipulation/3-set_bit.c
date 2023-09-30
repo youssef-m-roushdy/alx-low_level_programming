684bd21f1b10c8c0af50473447303ba46628bfa6
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-29 19:41:38 -0700

    100

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= 31)
+	if (index >= 63)
 	{
 		return (-1);
 	}
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int shift = 1 << index;
 
-	if (index > 31)
+	if (index > 63)
 	{
 		return (-1);
 	}
