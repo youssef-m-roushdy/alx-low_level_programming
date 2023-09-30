a5b7064ee340baa14fa888cd495baf5443e4b137
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2023-09-29 19:28:32 -0700

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+/**
+ * set_bit - Set the value of a specific bit in
+ * an unsigned long integer.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to set
+ * (0-based, rightmost bit is 0).
+ *
+ * Return: 1 if the operation is successful, or -1 if 'index'
+ * is out of bounds.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+	{
+		return (-1);
+	}
+	*n |= (1UL << index);
+	return (*n);
+}
